ace57e707631e126e9fe78511dd668ce17ce1486
wagners32 <wagnererlike@gmail.com>
2019-12-19 12:08:17 -0200

    Adicionada rota para a home

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users, param: :_username
   resources :purchases, only: %i[index edit create update destroy]
   
+  get '/', to: 'home#index'
   post '/auth/login', to: 'authentication#login'
   get '/cashback', to: 'purchases#cashback'
   get '/*a', to: 'application#not_found'
