0e839125940a7a2b5fcec5423fb66d5359994e72
wagners32 <wagnererlike@gmail.com>
2019-12-20 00:56:54 -0200

    Adicionado link do swaggerhub na resposta do /

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    render json: {message: 'API CashBack'}
+    render json: {message: 'API CashBack ( https://app.swaggerhub.com/apis-docs/apicashback7/api_cash_back/1.0.0-oas3 )'}
   end
 end
